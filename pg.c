4c4c426edcfa140f6243866c6408249dab122e30
Breno Gonçalves <124218590+Breno-G@users.noreply.github.com>
2026-04-01 13:42:43 -0300

    Refactor calculation methods and main loop

--- pg.c
+++ pg.c
@@ -2,33 +2,61 @@
 #include <math.h>
 #include <time.h>
 
+double calculaTempo(clock_t inicio, clock_t fim);
+double calculaTermoAtual(double primeiroTermo, double quociente, double base, int nTermos);
+double somaArray(double primeiroTermo, double quociente, double base, int nTermos);
+double calculaNovoElemento(double primeiroTermo, double quociente, double base, int nTermos);
+double calculaFormula(double primeiroTermo, double quociente, double base, int nTermos);
+
+int main()
+{
+    while(1){
+        double a1 = 1.5, q = 2.0 / 3.0, baseLog = 2.0/3.0;
+
+        int opcao, n = 999999;
+
+        double tempoArray = somaArray(a1, q, baseLog, n );
+        double tempoSomaElemento = calculaNovoElemento(a1, q, baseLog, n );
+        double tempoFormula = calculaFormula(a1, q, baseLog, n );
+
+        printf("\n%s\n", "------------------------------------------------------------");
+        printf("%-25s | %-20s\n", "Metodo de Calculo", "Tempo Decorrido(s)");
+        printf("%s\n", "--------------------------|--------------------------------");
+        printf("%-25s | %.10f\n", "Soma com Array", tempoArray);
+        printf("%-25s | %.10f\n", "Soma por Elemento", tempoSomaElemento);
+        printf("%-25s | %.10f\n", "Soma por Formula (PA)", tempoFormula);
+        printf("%s\n", "------------------------------------------------------------\n");
+
+        printf("Digite 0 para sair ou qualquer outro numero para continuar\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+        
+        if(opcao == 0){break;};
+    }
+    return 0;
+}
+
 double calculaTempo(clock_t inicio, clock_t fim){ 
     return ((double) (fim - inicio)) / CLOCKS_PER_SEC;
 }
 
-double calculaTermoGeral(double primeiroTermo, double quociente, int nTermos){
-    return primeiroTermo * pow(quociente, nTermos - 1);
+double calculaTermoAtual(double primeiroTermo, double quociente, double base, int nTermos){
+    double logaritmando = primeiroTermo * pow(quociente, nTermos - 1);
+    return log(logaritmando) / log(base);
 }
 
-double somaArray(
-    double (*calculaTermoGeral)(double, double, int), 
-    double (*calculaTempo)(clock_t, clock_t),
-    double primeiroTermo, 
-    double quociente, 
-    int nTermos){
-
+double somaArray(double primeiroTermo, double quociente, double base, int nTermos){
     clock_t tInicial, tFinal;
-    double arrayResultados[30];
+    double arrayResultados[nTermos];
     double somatorio = 0.0;
-    int i;
 
     tInicial = clock();
 
-    for (i = 1; i <= nTermos; i++){
-        arrayResultados[i-1] = calculaTermoGeral(primeiroTermo, quociente, i);
+    for (int i = 1; i <= nTermos; i++){
+        arrayResultados[i-1] = calculaTermoAtual(primeiroTermo, quociente, base, i);
     }
     
-    for (i = 0; i < nTermos ; i++){
+    for (int i = 0; i < nTermos ; i++){
         somatorio += arrayResultados[i];
     }
 
@@ -36,21 +64,14 @@ double somaArray(
     return calculaTempo(tInicial,tFinal);
 }
 
-double calculaNovoElemento(
-    double(*calculaTermoGeral)(double,double,int),
-    double(*calculaTempo)(clock_t,clock_t),
-    double primeiroTermo, 
-    double quociente, 
-    int nTermos)
-{
+double calculaNovoElemento(double primeiroTermo, double quociente, double base, int nTermos){
     clock_t tInicial, tFinal;
     double somatorio = 0.0;
-    int i;
     
     tInicial = clock();
 
-    for (i = 1; i <= nTermos; i++){
-        somatorio += calculaTermoGeral(primeiroTermo, quociente, i);
+    for (int i = 1; i <= nTermos; i++){
+        somatorio += calculaTermoAtual(primeiroTermo, quociente, base, i);
     }
 
     tFinal = clock();
@@ -58,35 +79,15 @@ double calculaNovoElemento(
     return calculaTempo(tInicial, tFinal);
 }
 
-double calculaFormula(
-    double (*calculaTempo)(clock_t,clock_t),
-    double primeiroTermo, 
-    double quociente, 
-    int nTermos){
+double calculaFormula(double primeiroTermo, double quociente, double base, int nTermos){
     clock_t tInicial, tFinal;
     
     tInicial = clock();
 
-    double somatorio =  primeiroTermo * (pow(quociente,nTermos-1)-1) / (quociente - 1);
-   
+    double logA1 = log(primeiroTermo) / log(base);
+    double resultado = (nTermos * logA1) + (double)(nTermos * (nTermos - 1)) / 2.0;
+
     tFinal = clock();
-
+    
     return calculaTempo(tInicial,tFinal); 
-}
-
-int main()
-{
-    double a1 = 3.0 / 2.0;
-    double q = 2.0 / 3.0;
-    int n = 30;
-
-    double tempoArray = somaArray(calculaTermoGeral, calculaTempo, a1, q, n);
-    double tempoSomaElemento = calculaNovoElemento(calculaTermoGeral, calculaTempo, a1, q, n);
-    double tempoFormula = calculaFormula(calculaTempo, a1, q, n);
-
-    printf("Tempo array: %.10f segundos \n", tempoArray);
-    printf("Tempo por elemento: %.10f segundo \n", tempoSomaElemento);
-    printf("Tempo formula: %.10f segundos \n", tempoFormula);
-
-    return 0;
 }
